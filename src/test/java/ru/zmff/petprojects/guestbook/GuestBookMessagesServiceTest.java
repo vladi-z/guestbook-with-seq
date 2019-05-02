d33db9c8ea0a4f7db07227c4158bac7f89efd27f
zmff <drone_2008@mail.ru>
2019-05-02 23:14:10 +0300

    replaced Java archive packaging for tests with Web archive

--- src/test/java/ru/zmff/petprojects/guestbook/GuestBookMessagesServiceTest.java
+++ src/test/java/ru/zmff/petprojects/guestbook/GuestBookMessagesServiceTest.java
@@ -4,11 +4,12 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
+import java.io.File;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,10 +17,15 @@ import static org.junit.Assert.assertEquals;
 public class GuestBookMessagesServiceTest {
 
     @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war")
                 .addClasses(GuestBookMessagesService.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/web.xml"), "web.xml")
+                .addAsWebResource(new File("src/main/webapp/guestbook.jsp"), "guestbook.jsp") // added to avoid file removal
+                .addAsWebResource(new File("src/main/webapp/index.jsp"), "index.jsp")
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/glassfish-web.xml"), "glassfish-web.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+        ;
     }
 
     @Inject
